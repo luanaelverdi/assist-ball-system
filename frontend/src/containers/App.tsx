52e7de275a2f9877d7bef1f6c698ab7a99045d3f
luanaelverdi <luelverdi02@gmail.com>
2025-01-14 00:23:19 -0300

    front de 0 y login

---
+++ frontend/src/containers/App.tsx
@@ -0,0 +1,10 @@
+import {Formulario} from '../components/Formulario'
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
+}
+export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import { AppRouter } from './containers/AppRouter'
+import  App  from './containers/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <App />
   </StrictMode>,
 )
